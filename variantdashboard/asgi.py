77b27da1fbb9cb592fac42cdca82deafebb0251c
sophia <sophia.johnson-2@postgrad.ac.uk>
2023-11-07 16:03:36 +0000

    add Django skeleton structure

---
+++ variantdashboard/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for variantdashboard project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'variantdashboard.settings')
+
+application = get_asgi_application()
