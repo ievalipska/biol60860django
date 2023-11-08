0d8b90da31a045557791a58a977465a00f2cf8e0
ieva <ieva.lipska@ouh.nhs.uk>
2023-11-08 15:51:27 +0000

    Fixed code for fixtures.

--- dashboard/input_conversion.py
+++ dashboard/input_conversion.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from django.core import serializers
 
 # Define the input and output file paths
 input_file = 'BRCA_data.tsv'
@@ -12,6 +13,9 @@ with open(input_file, 'r', newline='', encoding='utf-8') as tsvfile:
     for row in reader:
         data.append(row)
 
+
+
+
 # Write the data to a JSON file
 with open(output_file, 'w', encoding='utf-8') as jsonfile:
-    json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+    json.dump(s_data, jsonfile, ensure_ascii=False, indent=4)
