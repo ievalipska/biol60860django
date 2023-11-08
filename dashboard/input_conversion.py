f00c61bf8ea7042f53045b37a5ec39db59eae9ab
ieva <ieva.lipska@ouh.nhs.uk>
2023-11-08 12:34:16 +0000

    Created python file to convert tsv data to json.
    Executed file to produce json output.

---
+++ dashboard/input_conversion.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+# Define the input and output file paths
+input_file = 'BRCA_data.tsv'
+output_file = 'BRCA_data_converted.json'
+
+# Read the TSV file and convert it to a list of dictionaries
+data = []
+with open(input_file, 'r', newline='', encoding='utf-8') as tsvfile:
+    reader = csv.DictReader(tsvfile, delimiter='\t')
+    for row in reader:
+        data.append(row)
+
+# Write the data to a JSON file
+with open(output_file, 'w', encoding='utf-8') as jsonfile:
+    json.dump(data, jsonfile, ensure_ascii=False, indent=4)
