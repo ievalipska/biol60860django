e7cab51045013e9aeb1777c0e3c9a935202dda3c
sophia <sophia.johnson-2@postgrad.ac.uk>
2023-11-08 16:30:49 +0000

    Changes to get data to show in front-end

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,11 +4,7 @@ from .models import Variants
 
 def home(request):
     variants = Variants.objects.all()
-    
-    data = {
-        'variants': variants,
-    }
-    return render(request, 'dashboard/home.html', {'data': data})
+    return render(request, 'dashboard/home.html', {'variants': variants})
 
 def addvariant(request):
     return render(request, 'dashboard/addvariant.html')
