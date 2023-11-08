1daad8c4276a7fd7aa118eb58682840e1324f48e
ieva <ieva.lipska@ouh.nhs.uk>
2023-11-08 20:24:04 +0000

    Fixed table header error.

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,5 +6,6 @@ def home(request):
     variants = Variants.objects.all()
     return render(request, 'dashboard/home.html', {'variants': variants})
 
+
 def addvariant(request):
     return render(request, 'dashboard/addvariant.html')
