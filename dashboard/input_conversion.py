3368478234249d28acee0d623272b727d79abc3e
ieva <ieva.lipska@ouh.nhs.uk>
2023-11-08 19:15:58 +0000

    Removed unneccessary fixtures files temporarily.

--- dashboard/input_conversion.py
+++ dashboard/input_conversion.py
@@ -1,6 +1,5 @@
 import csv
 import json
-from django.core import serializers
 
 # Define the input and output file paths
 input_file = 'BRCA_data.tsv'
@@ -14,8 +13,16 @@ with open(input_file, 'r', newline='', encoding='utf-8') as tsvfile:
         data.append(row)
 
 
-
+new_data = []
+for i, item in enumerate(data):
+    new_data.append(
+        {
+        "pk": i,
+        "model": "dashboard:Variants",
+        "fields": item,
+        }
+    )
 
 # Write the data to a JSON file
 with open(output_file, 'w', encoding='utf-8') as jsonfile:
-    json.dump(s_data, jsonfile, ensure_ascii=False, indent=4)
+    json.dump(new_data, jsonfile, ensure_ascii=False, indent=4)
