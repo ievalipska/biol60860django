fe08441fd38e213ff4b94f08af35aa5ea79b9634
sophia <sophia.johnson-2@postgrad.ac.uk>
2023-11-08 11:55:52 +0000

    Fix gene FK

--- dashboard/models.py
+++ dashboard/models.py
@@ -14,22 +14,21 @@ class Sequencing_Run(models.Model):
     Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     Sequencer = models.CharField(max_length=100, null=True, blank=True)
     history = HistoricalRecords()
+    
+class Gene(models.Model):
+    id = models.AutoField(primary_key=True)
+    Gene_Symbol = models.CharField(max_length=50, null=True, blank=True)
+    history = HistoricalRecords()
 
 class Variant(models.Model):
     id = models.AutoField(primary_key=True)
     Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     Sequencing_Run = models.ForeignKey(Sequencing_Run, on_delete=models.CASCADE)
-    Gene =  models.ForeignKey(Gene, on_delete=models.CASCADE)
+    Gene = models.ForeignKey(Gene, on_delete=models.CASCADE)
     genomic_variant = models.CharField(max_length=100, null=True, blank=True)
     cDNA_variant = models.CharField(max_length=100, null=True, blank=True)
     protein_variant = models.CharField(max_length=100, null=True, blank=True)
     Classification = models.CharField(max_length=100, null=True, blank=True)
-    history = HistoricalRecords()
-
-
-class Gene(models.Model):
-    id = models.AutoField(primary_key=True)
-    Gene_Symbol = models.CharField(max_length=50, null=True, blank=True)
     history = HistoricalRecords()
 
 class Disease(models.Model):
@@ -39,4 +38,3 @@ class Disease(models.Model):
     Stage = models.IntegerField(null=True, blank=True)
     Description = models.CharField(max_length=50, null=True, blank=True)
     history = HistoricalRecords()
-    
