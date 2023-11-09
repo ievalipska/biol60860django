4b1399844b7de27c4bde3469c825a8363bb7fa0b
ieva <ieva.lipska@ouh.nhs.uk>
2023-11-09 11:31:10 +0000

    Added comments for filtering and search skeleton code.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     # Extract elements of URL and make them available as arguments to views.filtered"
-    re_path(r"^filtered/(?P<column>.+)/(?P<filter>.+)$", views.filtered, name="filtered"),
+    
 
     path('addvariant', views.addvariant, name='addvariant'),
     path('createvariant', views.createvariant, name='createvariant'),
